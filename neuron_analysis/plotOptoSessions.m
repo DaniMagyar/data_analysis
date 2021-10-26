f4fee75b036f9948f9ce2d593ddfd0f47a097868
ADS/dmagyar <magyar.dani@hotmail.com>
2021-10-26 13:22:28 -0400

    Revert "git checkout downloadmanager"
    This reverts commit 2549e2918035fe582943e4173ed27f18096d528c.

--- neuron_analysis/plotOptoSessions.m
+++
@@ -1,8 +0,0 @@
-savedFreqs = who('-file','optoTTL.mat');
-
-for ii = 1:numel(savedFreqs)
-    load('optoTTL.mat', savedFreqs{ii})
-    groups=(1:16);
-    allneurons_psth_dani_dat(groups, eval(savedFreqs{ii}), 'pre', 0.05, 'post', 0.05, 'bin', 60, 's', 'figname', savedFreqs{ii})
-end
-
--- neuron_analysis/saveXlsxAsMat.m
+++
@@ -1,4 +0,0 @@
-opts = detectImportOptions('PFC_Chrimson_stGtACR.xlsx');
-opts = setvartype(opts, 'Group', 'char');
-PFC_Chrimson_stGtACR = readtable('PFC_Chrimson_stGtACR.xlsx', opts);
-save ('PFC_Chrimson_stGtACR.mat', 'PFC_Chrimson_stGtACR')
